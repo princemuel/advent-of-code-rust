4b7fd31128d82ebf88ebb26e7fcf1ecb845b9550
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-12-04 09:46:24 +0100

    chore: init 2024

--- crates/aoc2024/src/prelude.rs
+++ crates/aoc2024/src/prelude.rs
@@ -1,13 +1,14 @@
 //! Convenience prelude for internal modules.
 //!
-//! modules can `use crate::prelude::*;` to pull in common
+//! modules can `use aoc2024::prelude::*;` to pull in common
 //! types and imports and avoid long `use` lists.
 
 pub use core::error::Error;
 pub use core::str::FromStr;
-pub use std::io::Read;
+pub use std::fs::File;
+pub use std::io::{BufRead, BufReader, Read};
 pub use std::path::{Path, PathBuf};
-pub use std::{fmt, fs, io};
+pub use std::{env, fmt, fs, io};
 
 /// Read all data from standard input into a string.
 ///
@@ -16,6 +17,6 @@ pub fn read_input() -> String {
     let mut buf = String::new();
     io::stdin()
         .read_to_string(&mut buf)
-        .expect("Failed to read buf");
-    buf.trim().to_string()
+        .expect("Failed to read stdin");
+    buf.trim().to_owned()
 }
